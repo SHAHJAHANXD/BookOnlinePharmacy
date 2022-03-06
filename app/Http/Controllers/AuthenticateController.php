3cbf271229e92d056002f2cf2dcd902ff33383f4
SHAH JAHAN <87589922+SHAHJAHANXD@users.noreply.github.com>
2022-03-06 10:53:23 +0500

    ok

---
+++ app/Http/Controllers/AuthenticateController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthenticateController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|unique:users|email',
+            'password' => 'required|confirmed|min:6',
+        ]);
+        if ($validator->fails()) {
+
+            return response()->json(['status' => false, 'errors' => $validator->errors()]);
+        }
+        $request->validate([]);
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        $response = ['status' => true, 'message' => "Account Created Successfully!"];
+        return response($response, 200);
+    }
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6',
+        ]);
+        if ($validator->fails()) {
+            return response(['status' => false, 'errors' => $validator->errors()->all()], 422);
+        }
+        $user = User::where('email', $request->email)->first();
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+                $response = ['status' => true, 'message' => "User LoggedIn success!", 'token' => $token, 'data' => $user];
+                return response($response, 200);
+            } else {
+                $response = ["message" => "Password mismatch"];
+                return response($response, 422);
+            }
+        } else {
+            $response = ["message" => 'User does not exist'];
+            return response($response, 422);
+        }
+    }
+}
